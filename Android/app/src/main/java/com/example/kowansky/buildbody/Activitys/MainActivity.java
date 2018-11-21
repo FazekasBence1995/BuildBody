3b2e1de906fdf5f7f06cb498d4a694256281e464
Fazekas Bence <fazekas.bence1995@gmail.com>
2018-11-21 13:50:49 +0100

    Ready Gifs

--- Android/app/src/main/java/com/example/kowansky/buildbody/Activitys/MainActivity.java
+++ Android/app/src/main/java/com/example/kowansky/buildbody/Activitys/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.example.kowansky.buildbody.Adapters.TrainingsListAdapter;
 import com.example.kowansky.buildbody.Application.ApiClient;
@@ -45,6 +48,25 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.signOut) {
+
+            singOut();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void myTrainingPlanPerformed() {
         cameMyTraining = true;
 
@@ -78,5 +100,10 @@ public class MainActivity extends AppCompatActivity implements WelcomeFragment.O
         bundle.putString("bodyPartsName",name);
         myTrainingsFragment.setArguments(bundle);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, myTrainingsFragment).addToBackStack(null).commit();
+    }
+
+    public void singOut(){
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
     }
 }
